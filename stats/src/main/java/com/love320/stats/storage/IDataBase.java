e89107b79bd9b01cf6a9a8d981854ced62a4b2aa
love320 <admin>
2015-04-27 17:45:38 +0800

    add

--- stats/src/main/java/com/love320/stats/storage/IDataBase.java
+++ stats/src/main/java/com/love320/stats/storage/IDataBase.java
@@ -2,6 +2,11 @@ package com.love320.stats.storage;
 
 import java.util.Map;
 
+/**
+ * 统计数据持久
+ * @author z{user}d
+ *
+ */
 public interface IDataBase {
 	
 	public boolean write(String table,Map<String,Object> data);
